2cc3b0374301425129545e1ccbdd5eb69859dd60
xxHANIxx <kimcm0319@gmail.com>
2019-04-15 12:47:11 +0900

    MODIFY FILE [Service.java, IService.java]
    1. JAVA 파일 수정
    
    - Service.java
    - IService.java
    - 파일위치 : 06. 소스코드\SFT_EDC_CNT\src\Teacher
    
    2. 성적입출력 부분 개발
    
    - 화면설계 PPT 기준 성적입출력 부분 작업
    - 성적입출력, 성적입력, 성적조회로 나뉨
    - 추후 DAO 작업 필요
    - 추후 UI 수정 작업 필요

--- 06. 소스코드/SFT_EDC_CNT/src/Teacher/IService.java
+++ 06. 소스코드/SFT_EDC_CNT/src/Teacher/IService.java
@@ -4,7 +4,13 @@ public interface IService {
 
 	void teacherSchedule() throws InterruptedException; // 강의스케줄 조회
 
-	void gradeOuput(); // 성적입출력
+	void studentInfo() throws InterruptedException; // 강의스케줄 조회 - 학생정보 조회
+	
+	void gradeInputOuput() throws InterruptedException; // 성적입출력
+	
+	void gradeInput() throws InterruptedException; // 성적 입력
+	
+	void gradeOutput() throws InterruptedException; // 성적 조회
 
 	void attendanceMGMT(); // 출결관리 및 조회
 
@@ -13,6 +19,6 @@ public interface IService {
 	void teacherConsult(); // 상담일지 입력 및 조회
 	
 	
-	void studentInfo() throws InterruptedException;
+
 	
 }
