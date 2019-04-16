8854d99ff203598936c52861f46f6c11a2454523
xxHANIxx <kimcm0319@gmail.com>
2019-04-16 14:13:49 +0900

    MODIFY FILE [Service(04_teacherEvaluation).java]
    1. JAVA 파일 수정
    
    - 반복 메시지 출력 현상 수정
    - 메뉴 화면으로 돌아오는 현상 수정

--- 06. 소스코드/SFT_EDC_CNT/src/Teacher/Service(04_teacherEvaluation).java
+++ 06. 소스코드/SFT_EDC_CNT/src/Teacher/Service(04_teacherEvaluation).java
@@ -1,11 +1,11 @@
 ﻿
 // Service - Teacher - teacherEvaluation
-	
+
+
 // ----------------------------------------------------------------------
 //		[4] 교사평가 조회  - (1) 메뉴
 //----------------------------------------------------------------------
 
-	
 	@Override
 	public void teacherEvaluation() throws InterruptedException { // 교사평가 메뉴
 
@@ -34,16 +34,19 @@
 
 		case 1: // 객관식 통계
 			str = "객관식 통계";
+			evalOutput(select, str); // 출력
 			break;
 
 		case 2: // 좋았던 점
 			flag = 1;
 			str = "좋았던 점";
+			evalOutput(select, str); // 출력
 			break;
 
 		case 3: // 아쉬운 점 & 개선할 점
 			flag = 1;
 			str = "아쉬운 점 & 개선할 점";
+			evalOutput(select, str); // 출력
 			break;
 
 		case 0: // 교사 메뉴로 돌아가기
@@ -52,8 +55,6 @@
 			break;
 		}
 
-		evalOutput(select, str); // 출력
-
 	}
 
 	
@@ -61,7 +62,6 @@
 //			[4] 교사평가 조회 - (2) 평가결과
 //----------------------------------------------------------------------
 
-		
 	@Override
 	public void evalOutput(int select, String str) throws InterruptedException {
 
@@ -79,52 +79,49 @@
 			System.out.println("--------------------------------------------------------------------");
 			System.out.println("DAO작업 필요");
 
-		} else if(select == 2 || select == 3) { // 주관식
+		} else if (select == 2 || select == 3) { // 주관식
 
 			System.out.println(str);
 			System.out.println("--------------------------------------------------------------------");
 			System.out.println("번호\t내용");
 			System.out.println("--------------------------------------------------------------------");
-			
+
 			if (flag == 1) { // 25명 중 1~10번까지 DAO 출력
-	
+
 				System.out.println("DAO 출력");
 				System.out.println("---------------------------------------------------------------");
 				System.out.println("\t\t(1..10 / 25)");
-	
+
 			} else if (flag == 2) { // 25명 중 11~20번까지 DAO 출력
-	
+
 				System.out.println("DAO 출력");
 				System.out.println("---------------------------------------------------------------");
 				System.out.println("\t\t(11..20 / 25)");
-	
+
 			} else if (flag == 3) { // 25명 중 21~25번까지 DAO 출력
-	
+
 				System.out.println("DAO 출력");
 				System.out.println("---------------------------------------------------------------");
 				System.out.println("\t\t(21..25 / 25)");
-			
+
 			}
 		}
-		
+
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("------------------------");
 		System.out.println("[  ] 안의 번호를 입력해주세요.");
-	
+
 		if (select == 1) { // 객관식 통계 화면
-			
+
 			System.out.println();
 			System.out.println("[1] 좋았던 점");
 			System.out.println("[2] 아쉬운 점 & 개선할 점");
 			System.out.println("[0] 교사 메뉴로 돌아가기");
-			System.out.println("------------------------");
-			System.out.print("번호 : ");
-			
-			
+
 		} else if (select == 2) { // 좋았던 점 화면
-			
+
 			System.out.println();
-			
+
 			if (flag == 1) {
 				System.out.println("[1] 다음 >>");
 				System.out.println("[2] 객관식 통계");
@@ -142,11 +139,11 @@
 				System.out.println("[4] << 이전");
 				System.out.println("[0] 교사 메뉴로 돌아가기");
 			}
-			
+
 		} else if (select == 3) { // 아쉬운 점 & 개선할 점 화면
-			
+
 			System.out.println();
-			
+
 			if (flag == 1) {
 				System.out.println("[1] 다음 >>");
 				System.out.println("[2] 객관식 통계");
@@ -164,67 +161,71 @@
 				System.out.println("[4] << 이전");
 				System.out.println("[0] 교사 메뉴로 돌아가기");
 			}
-			
+
 		}
-		
+
 		System.out.println("------------------------");
 		System.out.print("번호 : ");
-		String num = scan.nextLine();
+		int num = Integer.parseInt(scan.nextLine());
 		view.pause();
-		
-		switch(num) {
-		
-			case "1" :
-				if (select == 1) { // 객관식 통계 -> 좋았던 점
-					str = "좋았던 점"; 
-					select = 2;
-					flag = 1;
-				} else if (select == 2) { // 좋았던 점, 다음
-					str = "좋았던 점";
-					flag++;
-				} else if (select == 3) { // 아쉬운 점 & 개선할 점, 다음
-					str = "아쉬운 점 & 개선할 점"; 
-					flag++;
-				}		
-				break;
+
+		if (num == 1) {
+
+			if (select == 1) { // 객관식 통계 -> 좋았던 점
+				str = "좋았던 점";
+				select = 2;
+				flag = 1;
+			} else if (select == 2) { // 좋았던 점, 다음
+				str = "좋았던 점";
+				flag++;
+			} else if (select == 3) { // 아쉬운 점 & 개선할 점, 다음
+				str = "아쉬운 점 & 개선할 점";
+				flag++;
+			}
 			
-			case "2" :
-				if (select == 1) { // 객관식 통계 -> 아쉬운 점 &개선할 점
-					str = "아쉬운 점 & 개선할 점";
-					select = 3;
-					flag = 1;
-				} else if (select == 2 || select == 3) { // 좋았던 점, 아쉬운 점 -> 객관식 통계
-					str = "객관식 통계";
-					select = 1;
-				}
-				break;
+			evalOutput(select, str);
 			
-			case "3" :
-				if (select == 2) { // 좋았던 점 <-> 아쉬운 점 & 개선할 점
-					str = "아쉬운 점 & 개선할 점";
-					select = 3;
-					flag = 1;
-				} else if (select == 3) {
-					str = "좋았던 점";
-					select = 2;
-					flag = 1;
-				}
-				break;
+		} else if (num == 2) {
+
+			if (select == 1) { // 객관식 통계 -> 아쉬운 점 &개선할 점
+				str = "아쉬운 점 & 개선할 점";
+				select = 3;
+				flag = 1;
+			} else if (select == 2 || select == 3) { // 좋았던 점, 아쉬운 점 -> 객관식 통계
+				str = "객관식 통계";
+				select = 1;
+			}
 			
-			case "4" : // 이전
-				
-				if (select == 2) { str = "좋았던 점"; } 
-				else if (select == 3) { str = "아쉬운 점 & 개선할 점"; }
-				
-				flag --;
-				evalOutput(select, str);
-				break;
-				
-			case "0" : // 교사 메누로 돌아가기
-				flag = 0;
-				TeacherController.start();
-				break;
-		
+			evalOutput(select, str);
+			
+		} else if (num == 3) {
+			if (select == 2) { // 좋았던 점 <-> 아쉬운 점 & 개선할 점
+				str = "아쉬운 점 & 개선할 점";
+				select = 3;
+				flag = 1;
+			} else if (select == 3) {
+				str = "좋았던 점";
+				select = 2;
+				flag = 1;
+			}
+			
+			evalOutput(select, str);
+			
+		} else if (num == 4) {
+
+			if (select == 2) {
+				str = "좋았던 점";
+			} else if (select == 3) {
+				str = "아쉬운 점 & 개선할 점";
+			}
+
+			flag--;
+			evalOutput(select, str);
+			
+		} else if (num == 0) { // 교사 메누로 돌아가기
+
+			flag = 0;
+			TeacherController.start();
 		}
 
 	}
